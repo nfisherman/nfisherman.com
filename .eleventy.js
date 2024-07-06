e458f109807f8909114d4d966251b17bc7e78cab
nfisherman <123318218+nfisherman@users.noreply.github.com>
2024-07-06 14:04:24 -0500

    formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,26 @@
 module.exports = (eleventyConfig) => {
 	eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
-    eleventyConfig.addPassthroughCopy("./asset/");
-    eleventyConfig.addPassthroughCopy("./script/");
-    eleventyConfig.addPassthroughCopy("./style/")
-    eleventyConfig.addPassthroughCopy("./copyright");
-    eleventyConfig.addPassthroughCopy({
-        "./site/index.html": "./index.html"
-    });
-    eleventyConfig.addPassthroughCopy({
-        "./tools/pull-latest.sh": "./pull-latest.sh"
-    });
+	eleventyConfig.addPassthroughCopy("./asset/");
+	eleventyConfig.addPassthroughCopy("./script/");
+	eleventyConfig.addPassthroughCopy("./style/")
+	eleventyConfig.addPassthroughCopy("./copyright");
+	eleventyConfig.addPassthroughCopy({
+		"./site/index.html": "./index.html"
+	});
+	eleventyConfig.addPassthroughCopy({
+		"./tools/pull-latest.sh": "./pull-latest.sh"
+	});
 
 	eleventyConfig.addExtension("11ty.php", {
 		key: "11ty.html",
 	});
 
-    return {
-        dir: {
-            input: "site",
-            includes: "_includes",
+	return {
+		dir: {
+			input: "site",
+			includes: "_includes",
 			layouts: "_layouts",
-            output: "dist",
-        },
-    };
+			output: "dist",
+		},
+	};
 };
